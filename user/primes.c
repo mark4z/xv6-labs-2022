db40d6eaadcc44a4cc473849301d2f9851e86b5d
mark4z <mark4z@apache.org>
2023-05-09 00:42:38 +0800

    primes

--- user/primes.c
+++ user/primes.c
@@ -9,19 +9,17 @@ main(int argc, char *argv[]) {
 
     int child = fork();
     if (child == 0) {
-        uint32_t num;
-        while (read(fd[1], &num, sizeof(num)) != 0) {
+        int num;
+        while (read(fd[0], &num, sizeof(num)) != 0) {
             fprintf(1, "prime %d\n", num);
         }
         exit(0);
     } else {
-        close(fd[1]);
-        for (int i = 2; i < 35; ++i) {
-            uint32_t *num = malloc(sizeof(uint32_t));
-            *num = i;
-            write(fd[0], num, sizeof(num));
-        }
         close(fd[0]);
+        for (int i = 2; i < 35; ++i) {
+            write(fd[1], &i, sizeof(int));
+        }
+        close(fd[1]);
         wait(0);
     }
 
