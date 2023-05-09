43d96332dd62a3d9ed2327173b11e154973eab6b
mark4z <mark4z@apache.org>
2023-05-10 00:06:12 +0800

    17

--- user/primes.c
+++ user/primes.c
@@ -8,62 +8,91 @@ int
 main(int argc, char *argv[]) {
     int fd[2];
     pipe(fd);
-
-    int child = fork();
-    if (child == 0) {
-        dup(fd[0]);
-        dup(fd[1]);
-
+    int rc = fork();
+    if (rc < 0) {
+        return 1;
+    } else if (rc == 0) {
         close(fd[1]);
-        close(fd[0]);
-
         int num;
-        if (read(4, &num, sizeof(num)) != 0) {
-            fprintf(1, "prime %d\n", num);
+        int in = fd[0];
+        if (read(in, &num, sizeof(num)) == 0) {
+            exit(0);
         }
-        while (read(4, &num, sizeof(num)) != 0) {
-            write(5, &num, sizeof(int));
-        }
+        fprintf(1, "prime %d\n", num);
+
+        int fdNew[2];
+        pipe(fdNew);
+        int nextIn = fdNew[0];
+        int out = fdNew[1];
+
+        close(0);
+        dup(nextIn);
         primes();
+        close(0);
+        close(nextIn);
+
+        int pass;
+        while (read(in, &pass, sizeof(pass)) != 0) {
+            if (pass % num != 0) {
+                write(out, &pass, sizeof(int));
+            } else {
+            }
+        }
+        close(out);
+        close(in);
+        wait(0);
+
         exit(0);
     } else {
+        close(0);
         close(fd[0]);
         for (int i = 2; i < 35; ++i) {
             write(fd[1], &i, sizeof(int));
         }
         close(fd[1]);
         wait(0);
+
+        exit(0);
     }
-
-
-    exit(0);
 }
 
 
 void primes() {
     int child = fork();
-    if (child == 0) {
+    if (child < 0) {
+        exit(1);
+    } else if (child == 0) {
+        close(3);
         dup(0);
-        dup(1);
-        close(0);
-        close(1);
-        dup(5);
-        dup(4);
-        close(4);
-        close(5);
+        int in = 3;
+
         int num;
-        if (read(0, &num, sizeof(num)) != 0) {
-            fprintf(1, "prime %d\n", num);
-        } else {
+        if (read(in, &num, sizeof(num)) == 0) {
             exit(0);
         }
-        while (read(0, &num, sizeof(num)) != 0) {
-            write(1, &num, sizeof(int));
-        }
+        fprintf(1, "prime %d\n", num);
+
+        int fd[2];
+        pipe(fd);
+        int newIn = fd[0];
+        int out = fd[1];
+
+        close(0);
+        dup(newIn);
         primes();
         close(0);
-        close(1);
+
+        close(newIn);
+        int pass;
+        while (read(in, &pass, sizeof(pass)) != 0) {
+            if (pass % num != 0) {
+                write(out, &pass, sizeof(int));
+            }
+        }
+        close(in);
+        close(out);
+        wait(0);
+
         exit(0);
     }
-    wait(0);
 }
