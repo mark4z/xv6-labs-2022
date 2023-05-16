5af88f43defc2c01a0d01e1c230c7ccc5e0de21f
mark4z <mark4z@apache.org>
2023-05-16 23:43:11 +0800

    sysinfotest.c

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,16 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+int freemem(void) {
+    int cnt = 0;
+
+    acquire(&kmem.lock);
+    struct run *start = kmem.freelist;
+    while (start) {
+        cnt++;
+        start = start->next;
+    }
+    release(&kmem.lock);
+    return cnt * PGSIZE;
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -683,3 +683,17 @@ procdump(void)
     printf("\n");
   }
 }
+
+int nproc(void) {
+    int cnt = 0;
+    struct proc *p;
+
+    for (p = proc; p < &proc[NPROC]; p++) {
+        acquire(&p->lock);
+        if (p->state != UNUSED) {
+            cnt++;
+        }
+        release(&p->lock);
+    }
+    return cnt;
+}
